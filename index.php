c79b0a3c590ce0b9850359350f5cb30bfed8cafa
mbalcer <mateusz75319@gmail.com>
2019-02-26 21:37:25 +0100

    First commit

---
+++ index.php
@@ -0,0 +1,54 @@
+<!doctype html>
+<html lang="pl">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Service Management</title>
+    <link rel="stylesheet" href="css/main.css">
+</head>
+<body>
+    <header>
+        Service Management
+    </header>
+    <main>
+        <section class="add-fault">
+            <form action="">
+                <div class="form-group">
+                    <label for="clientName">Client name:</label>
+                    <input type="text" name="clientName">
+                </div>
+
+                <div class="form-group">
+                    <label for="phone">Phone: </label>
+                    <input type="text" name="phone">
+                </div>
+
+                <div class="form-group">
+                    <label for="email">Email:</label>
+                    <input type="text" name="email">
+                </div>
+
+                <div class="form-group">
+                    <label for="description">Description: </label>
+                    <input type="text" name="description">
+                </div>
+
+                <div class="form-group">
+                    <input type="submit" name="btnAdd" value="Add">
+                </div>
+            </form>
+        </section>
+    </main>
+</body>
+</html>
+<?php
+/**
+ * Created by PhpStorm.
+ * User: mateusz
+ * Date: 26.02.19
+ * Time: 19:06
+ */
+
+?>
