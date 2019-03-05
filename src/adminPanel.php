a5f6b65c7e71cfe5a6bcae22f592dda6bf730676
mbalcer <mateusz75319@gmail.com>
2019-03-05 19:50:45 +0100

    I created a login panel

---
+++ src/adminPanel.php
@@ -0,0 +1,88 @@
+<?php
+    require_once 'DatabaseConnector.php';
+    require_once 'User.php';
+    require_once 'Hardware.php';
+    session_start();
+?>
+
+<!doctype html>
+<html lang="pl">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Service Management</title>
+    <link rel="stylesheet" href="css/main.css">
+    <link rel="stylesheet" href="css/fontello.css">
+</head>
+<body>
+    <header>
+        Service Management
+    </header>
+    <main>
+        <section class="add-fault">
+            <form action="addNewFault.php" method="post">
+                <div class="form-group">
+                    <label for="clientName">Client name:
+                    <input type="text" name="clientName"></label>
+                </div>
+
+                <div class="form-group">
+                    <label for="phone">Phone:
+                    <input type="text" name="phone"></label>
+                </div>
+
+                <div class="form-group">
+                    <label for="email">Email:
+                    <input type="text" name="email"></label>
+                </div>
+
+                <div class="form-group">
+                    <label for="description">Description:
+                    <input type="text" name="description"></label>
+                </div>
+
+                <div class="form-group">
+                    <input type="submit" name="btnAdd" value="Add" class="btn-main">
+                </div>
+            </form>
+            <?php
+                if(isset($_SESSION['info'])) {
+                    echo '<div class="info">' . $_SESSION['info'] . '</div>';
+                    unset($_SESSION['info']);
+                }
+            ?>
+        </section>
+
+        <section class="customer-table">
+            <table>
+                <thead>
+                <tr>
+                    <td>ID</td>
+                    <td>Client name</td>
+                    <td>Phone</td>
+                    <td>Email</td>
+                    <td>Description</td>
+                    <td>Status</td>
+                    <td>Price</td>
+                    <td class="action-in-row"></td>
+                </tr>
+                </thead>
+                <tbody>
+                    <?php
+                        $objHardware = new Hardware;
+                        echo $objHardware->getHardwareTable();
+                    ?>
+                </tbody>
+            </table>
+
+
+        </section>
+    </main>
+
+    <footer>
+        @Copyright Mateusz Balcer
+    </footer>
+</body>
+</html>
--- src/index.php
+++ src/index.php
@@ -1,10 +1,3 @@
-<?php
-    require_once 'DatabaseConnector.php';
-    require_once 'User.php';
-    require_once 'Hardware.php';
-    session_start();
-?>
-
 <!doctype html>
 <html lang="pl">
 <head>
@@ -13,7 +6,7 @@
           content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Service Management</title>
-    <link rel="stylesheet" href="css/main.css">
+    <link rel="stylesheet" href="css/loginPanel.css">
     <link rel="stylesheet" href="css/fontello.css">
 </head>
 <body>
@@ -21,68 +14,19 @@
         Service Management
     </header>
     <main>
-        <section class="add-fault">
-            <form action="addNewFault.php" method="post">
-                <div class="form-group">
-                    <label for="clientName">Client name:
-                    <input type="text" name="clientName"></label>
-                </div>
-
-                <div class="form-group">
-                    <label for="phone">Phone:
-                    <input type="text" name="phone"></label>
-                </div>
-
-                <div class="form-group">
-                    <label for="email">Email:
-                    <input type="text" name="email"></label>
-                </div>
-
-                <div class="form-group">
-                    <label for="description">Description:
-                    <input type="text" name="description"></label>
-                </div>
-
-                <div class="form-group">
-                    <input type="submit" name="btnAdd" value="Add" class="btn-main">
-                </div>
-            </form>
-            <?php
-                if(isset($_SESSION['info'])) {
-                    echo '<div class="info">' . $_SESSION['info'] . '</div>';
-                    unset($_SESSION['info']);
-                }
-            ?>
-        </section>
-
-        <section class="customer-table">
-            <table>
-                <thead>
-                <tr>
-                    <td>ID</td>
-                    <td>Client name</td>
-                    <td>Phone</td>
-                    <td>Email</td>
-                    <td>Description</td>
-                    <td>Status</td>
-                    <td>Price</td>
-                    <td class="action-in-row"></td>
-                </tr>
-                </thead>
-                <tbody>
-                    <?php
-                        $objHardware = new Hardware;
-                        echo $objHardware->getHardwareTable();
-                    ?>
-                </tbody>
-            </table>
-
-
-        </section>
+        <form action="login.php" method="post">
+            <div class="field">
+                <label for="email">Email</label>
+                <input type="text" name="email" class="field-input">
+            </div>
+            <div class="field">
+                <label for="password">Password</label>
+                <input type="password" name="password" class="field-input">
+            </div>
+            <div class="field">
+                <input type="submit" value="Login" name="login" class="btn-main">
+            </div>
+        </form>
     </main>
-
-    <footer>
-        @Copyright Mateusz Balcer
-    </footer>
 </body>
 </html>
