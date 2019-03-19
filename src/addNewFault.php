415c4e7039393ed0a09af13646327f68d364b378
mbalcer <mateusz75319@gmail.com>
2019-03-19 19:34:05 +0100

    Added generate password to new user

--- src/addNewFault.php
+++ src/addNewFault.php
@@ -5,7 +5,7 @@ require_once 'includes/DatabaseConnector.php';
 require_once 'includes/User.php';
 require_once 'includes/Hardware.php';
 
-$passwordUser = "newpass123"; //TODO create a password generator
+$passwordUser = bin2hex(openssl_random_pseudo_bytes(4));
 
 $client = new User;
 $info = $client->addNewUser($_POST['clientName'], $_POST['email'], $passwordUser, $_POST['phone']);
@@ -17,7 +17,7 @@ if($info!="I can't add new user") {
     $info = $hardware->addHardware($clientID, $_POST['description']);
 }
 
-$_SESSION['info'] = $info;
+$_SESSION['info'] = $info."<br> Password user is: ".$passwordUser;
 
 //TODO send email to client with data to account
 //TODO generate pdf to print with data to account
