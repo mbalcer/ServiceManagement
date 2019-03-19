a43765a56d45d9d784eaf1d67dbbd41383f37f92
mbalcer <mateusz75319@gmail.com>
2019-03-19 22:16:08 +0100

    Introduced send email after added new fault

--- src/addNewFault.php
+++ src/addNewFault.php
@@ -4,6 +4,7 @@ session_start();
 require_once 'includes/DatabaseConnector.php';
 require_once 'includes/User.php';
 require_once 'includes/Hardware.php';
+require_once 'includes/Email.php';
 
 $passwordUser = bin2hex(openssl_random_pseudo_bytes(4));
 
@@ -19,7 +20,8 @@ if($info!="I can't add new user") {
 
 $_SESSION['info'] = $info."<br> Password user is: ".$passwordUser;
 
-//TODO send email to client with data to account
+$email = new Email;
+$email->sendEmail($_POST['name'], $passwordUser, $_POST['email']);
 //TODO generate pdf to print with data to account
 
 header("Location: adminPanel.php"); // TODO redirect to ...
---
+++ src/includes/Email.php
@@ -0,0 +1,32 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: mateusz
+ * Date: 19.03.19
+ * Time: 19:45
+ */
+
+class Email
+{
+    public function sendEmail($name, $password, $email) {
+
+	    $headlines = "Reply-to: Service Management<contact@mbalcer.cba.pl>".PHP_EOL;
+        $headlines .= "From: Service Management <contact@mbalcer.cba.pl>".PHP_EOL;
+        $headlines .= "MIME-Version: 1.0".PHP_EOL;
+        $headlines .= "Content-type: text/html; charset=UTF-8".PHP_EOL;
+
+        $title = "Service Management - A repair request adopted";
+
+        $content = "Hello ".$name." <br><br> Your equipment repair request has been accepted <br>".
+            "You can follow the repair status by logging in to the website. <a href='http://mbalcer.cba.pl/ServiceManagement'> Link </a><br><br>".
+            "Your login data is: <br> ".
+            "Email: ".$email."<br>".
+            "Password: ".$password."<br>".
+            "<br> Greetings <br> Admin Service Management";
+
+
+        if(!(mail($email, $title, $content, $headlines)))
+            $_SESSION['info'] = "Email nie został wysłany. Skontaktuj się z supportem";
+
+    }
+}
