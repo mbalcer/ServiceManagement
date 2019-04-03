8055536c62fad0d92c4c8747a2044dcf31a56fd1
mbalcer <mateusz75319@gmail.com>
2019-04-03 20:56:33 +0200

    Created script to remove user

--- src/removeUser.php
+++ src/removeUser.php
@@ -1,1 +1,28 @@
 <?php
+
+if (!isset($_POST['id']))
+    header("Location: index.php");
+
+session_start();
+require_once 'includes/DatabaseConnector.php';
+require_once 'includes/User.php';
+require_once 'includes/Hardware.php';
+require_once 'includes/RepairStatus.php';
+
+$userID = $_POST['id'];
+
+$objHardware = new Hardware;
+$objUser = new User;
+$objRepairStatus = new RepairStatus;
+
+$allUserHardware = $objHardware->getHardwareForClientPanel($userID);
+
+foreach ($allUserHardware as $item) {
+    $objRepairStatus->removeStatusHistory($item['ID']);
+    $objHardware->removeHardware($item['ID']);
+}
+
+$result = $objUser->removeUser($userID);
+$_SESSION['info'] = $result;
+
+header("Location: manageUsers.php");
